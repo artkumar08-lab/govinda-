f0f2f9702758586351456cb295ad9c046d10d007
tirkumar58-sketch <tirkumar58@gmail.com>
2026-02-05 14:50:47 +0530

    Update and rename COUPLE-DP.js to 0👑coupledp.js

--- Priyansh/commands/0👑coupledp.js
+++ Priyansh/commands/0👑coupledp.js
@@ -20,6 +20,12 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
     var link = ["https://i.imgur.com/8eN11jn.jpeg","https://i.imgur.com/PDqskKV.jpeg","https://i.imgur.com/nV3Q6Kq.jpeg","https://i.imgur.com/8qdGqIn.jpeg","https://i.imgur.com/s29VPeP.jpeg","https://i.imgur.com/HmQzHbA.jpeg","https://i.imgur.com/zgnabGo.jpeg","https://i.imgur.com/qYVR8Py.jpeg","https://i.imgur.com/ErKmVZC.jpeg","https://i.imgur.com/CsIOsau.jpeg","https://i.imgur.com/RMiqnJ1.jpeg","https://i.imgur.com/W3sneFu.jpeg","https://i.imgur.com/gHZzUVv.jpeg","https://i.imgur.com/zTZfruL.jpeg","https://i.imgur.com/B7UDl8t.jpeg","https://i.imgur.com/mpsJ86M.jpeg","https://i.imgur.com/vTIDwRU.jpeg","https://i.imgur.com/rcRt24D.jpeg","https://i.imgur.com/SZeqU7M.jpeg","https://i.imgur.com/hzyzjRP.jpeg","https://i.imgur.com/FzpLRi4.jpeg","https://i.imgur.com/8jaMkFQ.jpeg","https://i.imgur.com/GuA1e9O.jpeg","https://i.imgur.com/IpmrSpU.jpeg","https://i.imgur.com/qInSLdo.jpeg","https://i.imgur.com/u6jGXWb.jpeg"           ];
-     var callback = () => api.sendMessage({body:`💝💝💝`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:`┏━━━━━━━━━━━━━┓
+  𓋜ྀི─୨⍤⃝𝗖𝗼𝘂𝗽𝗹𝗲 𝗱𝗽 ─˚˖𓍢ִ໋🦢˚
+      🎀✴⭐⭐✴🎀  
+         ┊                 ┊   
+         ┊                 ┊   
+  𓋜─୨⍤⃝𝗖𝗼𝘂𝗽𝗹𝗲 𝗱𝗽 ─˚🎀💗᪲᪲᪲
+┗━━━━━━━━━━━━━┛`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
